5a978760c0c2d9573ad77d55574ead27ffc57d89
Anuj Bhai Mehta <mehta.anuj2309@gmail.com>
2017-09-14 19:28:30 +0530

    changed logo

--- check_name.php
+++ check_name.php
@@ -1,14 +1,11 @@
 <?php
-/*
-Site	:	WWW.FEWPRESS.COM
-Author	:	Md. Rokonuzzaman Rokon
- * 
- */
+
  require_once 'config.php';
 
  if(!empty($_POST['name'])){
      $name=$_POST['name'];
-     $res=mysql_query("select count(user_name) as count from users where user_name='$name'") or die(mysql_error()); 
+     //$res=mysql_query("select count(user_name) as count from users where user_name='$name'") or die(mysql_error()); 
+     $res = mysql_query("select count from users where user_name='$name'") or die(mysql_error()); 
      $count=mysql_fetch_array($res);
      if($count[0]==0){
          echo 'true';
